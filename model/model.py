1ca49cf39b5868996f4c0d81a4e691ad7670bccb
agorodetskiy <gorodetskiyandrew@gmail.com>
2019-08-12 15:22:00 +0300

    added backtrace

--- model/model.py
+++ model/model.py
@@ -3,13 +3,14 @@ import numpy as np
 
 class SchemaNetwork:
     def __init__(self, N, M, A, L, T):
-        '''
+        """
         N: number of entities
         M: number of attributes of each entity
         A: number of available actions
         L: number of schemas
         T: size of look-ahead window
-        '''
+        required_actions: actions agent should take to reach planned reward
+        """
         self._N = N
         self._M = M
         self._A = A
@@ -24,13 +25,15 @@ class SchemaNetwork:
         # second - negative reward
         self._R = []
 
-    def fit(self):
-        pass
+        self.required_actions = []
+
+    def _reset_actions(self):
+        self.required_actions.clear()
 
     def _predict_next_attributes(self, X):
-        '''
+        """
         X: matrix of entities, [N x (MR + A)]
-        '''
+        """
         # check dimensions
         assert (X.shape == (self._N, (self._M * self._R + self._A)))
         for W in self._W:
@@ -46,9 +49,9 @@ class SchemaNetwork:
         return prediction
 
     def _predict_next_rewards(self, X):
-        '''
+        """
         X: vector of all attributes and actions, [1 x (NM + A)]
-        '''
+        """
         # check dimensions
         assert (X.shape == (1, (self._N * self._M + self._A)))
         for R in self._R:
@@ -64,10 +67,10 @@ class SchemaNetwork:
         return tuple(predictions)
 
     def _forward_pass(self, X, V):
-        '''
+        """
         X: matrix [N x (MR + A)]
         V: matrix [1 x (MN + A)]
-        '''
+        """
         current_state = X
         for t in range(self._T):
             next_state = self._predict_next_attributes(current_state)
@@ -75,25 +78,37 @@ class SchemaNetwork:
             # to construct it we need to measure distance between entities (to make [MR] part of the vector)
             # we need position attributes!
 
-    # draft
-    def _backtrace_attribute(v):
-        # label v as discovered
+    def _backtrace_attribute(self, node):
+        """
+        is_reachable: can it be activated under constraints of initial distribution at t = 0
+        """
+        node.is_discovered = True
+        if node.value is not None:
+            # this node is at t = 0, say back that it can be activated
+            return node.value
 
+        # lazy combining schemas by OR -> assuming False
         is_attr_reachable = False
 
-        for schema_idx in all_schemas_for_this_attr:
+        # dfs over schemas
+        for schema in node.schemas:
+            # lazy combining preconditions by AND -> assuming True
             is_schema_reachable = True
-            for all_preconditions in curr_schema:
-                if vertex_w is not labeled_as_discovered:
-                    is_precond_reachable = self._backtraice_attribute(w)
-                    if not is_precond_reachable:
-                        # schema can NEVER be reachable, break and try another schema
+            for precondition in schema.attribute_preconditions:
+                if not precondition.is_discovered:
+                    # dfs over precondition's schemas
+                    is_precondition_reachable = self._backtrace_attribute(precondition)
+                    if not is_precondition_reachable:
+                        # schema can *never* be reachable, break and try another schema
                         is_schema_reachable = False
                         break
 
             if is_schema_reachable:
-                # break loop (attr is already reachable)
+                # attribute is reachable by this schema
+                self.required_actions.append(schema.action_preconditions)
                 is_attr_reachable = True
                 break
+            else:
+                self._reset_actions()
 
         return is_attr_reachable
