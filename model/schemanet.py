0ad076fbe5e89868b12d7ff6c0c68f2998727b17
aleksandrashlychkova <aleksandrashlychkova@gmail.com>
2019-08-26 12:05:40 +0300

    edit add torch solution for lp

--- model/schemanet.py
+++ model/schemanet.py
@@ -1,10 +1,18 @@
 import numpy as np
-from scipy.optimize import linprog
+from scipy.optimize import linprog, minimize
 from tqdm import tqdm
 import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def binarize(x):
+    return 1 - ((x < 0.1) & (x > -0.1))
+
 
 class SchemaNet:
-    def __init__(self, N=0, M=53, A=2, L=100, window_size=2):
+    def __init__(self, N=0, M=53, A=2, L=5, window_size=2):
         self._M = M
         self.neighbour_num = (window_size * 2 + 1) ** 2
         print('neighbour_num_', self.neighbour_num)
@@ -17,13 +25,20 @@ class SchemaNet:
         self.memory = []
 
     def log(self):
-        print('current net:\n', [self._W[i].shape for i in range(len(self._W))])
+        print('current net shape:\n', [self._W[i].shape for i in range(len(self._W))])
+
+    def print(self):
+        print('current net:\n')
+        for i in range(len(self._W)):
+            print('   '*i, self._W[i].T)
 
     def predict_attr(self, X, i):
         X = np.array(X)
         if len(self._W[i].shape) == 1:
-            return (X == 0) @ self._W[i] == 0
-        return ((X == 0) @ self._W[i] == 0).any(axis=1) != 0
+            return 1 - binarize((X == 0) @ self._W[i])
+        #print('1', (1-binarize((X == 0) @ self._W[i])).any(axis=1) != 0)
+        #print('0', (((X == 0) @ self._W[i]) == 1).any(axis=1) != 0)
+        return (1 - binarize((X == 0) @ self._W[i])).any(axis=1) != 0
 
     def predict(self, X):
         return [self.predict_attr(X, i) for i in range(self._M)]
@@ -31,26 +46,62 @@ class SchemaNet:
     def add(self, schemas, i):
         self._W[i] = np.vstack((self._W[i].T, schemas.T)).T
 
-    def scipy_solve_lp(self, zero_pred, c, A_ub, b_ub, A_eq, b_eq, options={'maxiter': 2, "disp": False}):
-        if len(zero_pred) == 0:
+    def scipy_solve_lp(self, c, A_ub, b_ub, A_eq, b_eq, options={'maxiter': 2, "disp": False}):
+        if len(A_eq) == 0:
             return linprog(c=c, A_eq=A_eq, b_eq=b_eq, options=options).x.round(2)
         else:
             return linprog(c=c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, options=options).x.round(2)
 
-    """def torch_solve_lp(self, zero_pred, c, A_ub, b_ub, A_eq, b_eq):
+    def torch_solve_lp(self, c, A_ub, b_ub, A_eq, b_eq, num_steps=20, p=3):
+
+        torch_c = torch.tensor(c, dtype=torch.float32)
+        torch_A_ub = torch.tensor(A_ub, dtype=torch.float32)
+        torch_b_ub = torch.tensor(b_ub, dtype=torch.float32)
+        torch_A_eq = torch.tensor(A_eq, dtype=torch.float32)
+        torch_b_eq = torch.tensor(b_eq, dtype=torch.float32)
+
+        '''torch_c = c
+        torch_A_ub = A_ub
+        torch_b_ub = b_ub
+        torch_A_eq = A_eq
+        torch_b_eq = b_eq'''
+
+        INF = 10.
+
+        starting_point = torch.tensor(np.zeros(len(c)), dtype=torch.float32)
+        starting_point = Variable(starting_point, requires_grad=True)
+
+        optimizer = optim.SGD([starting_point], lr=1e-1)
 
         def function_to_optim(x):
-            x*c
+            # g = ().max()
+            # A_ub * x <= b_ub
+            # b_ub - A_ub * x >= 0
+            # (max(0, -g_i))^p
+            # g_i >= 0
+            #
+            g = (F.relu(torch_A_ub @ x - torch_b_ub)**p).sum()
+            h = ((torch_A_eq @ x - torch_b_eq)**2).sum()
+            print('debug', g, h)
+            return x @ torch_c + g + h
 
-        opt_func = lambda x:
+        for t in range(num_steps):
+            optimizer.zero_grad()
+            loss = function_to_optim(starting_point)
+            loss.backward()
+            optimizer.step()
 
-        #c = torch.tensor((1 - ones_pred).sum(axis=0))
-        #A_eq = torch.tensor(1 - self.solved)
-        #b_eq = torch.tensor(np.zeros(self.solved.shape[0]))
-        #A_ub = torch.tensor(zero_pred - 1)
-        #b_ub = torch.tensor(np.zeros(zero_pred.shape[0]) - 1)"""
+        #print('\nsolved', self.solved)
+        #print(A_ub, '\nb', b_ub)
+        #print(A_eq, '\nb', b_eq )
+        #print('predict:', self.predict_attr(X, 5))
+        #print(torch_A_ub@starting_point)
+        w = starting_point.detach().numpy()**2 > 1
+        #print('w', starting_point)
+        #print('w', w)
+        #print('check solved', binarize(A_eq@w), A_eq@w)
 
-
+        return w
 
     def get_not_predicted(self, X, y, i):
         ind = (self.predict_attr(X, i) != y) | (y == 0)
@@ -58,6 +109,8 @@ class SchemaNet:
 
     def get_next_to_predict(self, X, y, i):
         ind = (self.predict_attr(X, i) != y) * (y == 1)
+
+        #print ((self.predict_attr(X, i) != y) * (y == 0))
         return X[ind][0]
 
     def get_schema(self, X, y, i):
@@ -72,10 +125,15 @@ class SchemaNet:
         b_eq = np.zeros(self.solved.shape[0])
         A_ub = zero_pred - 1
         b_ub = np.zeros(zero_pred.shape[0]) - 1
-        # optimisation is looooooooooooooooong
-        w = self.scipy_solve_lp(zero_pred, c, A_ub, b_ub, A_eq, b_eq)
 
-        preds = ((X == 0) @ w) == 0
+        w = self.scipy_solve_lp(c, A_ub, b_ub, A_eq, b_eq)
+
+        #print( '\nresult', 1 - binarize((X == 0) @ w))
+        #print('\nwtf', (1 - self.solved)@w)
+
+        preds = 1 - binarize((X == 0) @ w)
+        #print((self.predict_attr(X, i) == 0)*preds)
+
         self.solved = np.vstack((self.solved, X[preds * (self.predict_attr(X, i) == 0)]))
         return w
 
@@ -83,15 +141,23 @@ class SchemaNet:
 
         zero_pred = X[y == 0]
 
-        c = np.zeros(self.neighbour_num * self._M + self._A)
+        c = np.zeros(self.neighbour_num * self._M + self._A) + 1
         A_eq = (1 - self.solved)
         b_eq = np.zeros(self.solved.shape[0])
         A_ub = (zero_pred - 1)
         b_ub = np.zeros(zero_pred.shape[0]) - 1
+        w = self.scipy_solve_lp(c, A_ub, b_ub, A_eq, b_eq)
 
-        return self.scipy_solve_lp(zero_pred, c, A_ub, b_ub, A_eq, b_eq)
+        #print('WTF', 1 - binarize((X == 0) @ w))
+        #print(w)
+
+        return w
 
     def fit(self, X, Y, log=True):
+
+        ind = X.sum(axis=1) > 0
+        X = X[ind]
+        Y = (Y.T[ind]).T
 
         for i in tqdm(range(self._M)):
 
@@ -104,12 +170,12 @@ class SchemaNet:
 
                 x, y = self.get_not_predicted(X, Y[i], i)
 
-                self.get_schema(x, y, i)
-                w = (self.simplify_schema(x, y) > 0.1) * 1
+                w = binarize(self.get_schema(x, y, i))
+                print('w:', w)
+                w_ = (self.simplify_schema(x, y) > 0.1) * 1
                 self.add(w, i)
                 if log:
                     self.log()
-
 
 
 if __name__ == '__main__':
@@ -118,14 +184,16 @@ if __name__ == '__main__':
                   [0, 0, 0, 0, 1, 0],
                   [1, 1, 1, 0, 0, 0],
                   [0, 0, 0, 1, 1, 0]])
+
     y = np.array([[0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0]])
+                  [1, 0, 0, 0, 1]])
     schemanet = SchemaNet(M=6, A=0, window_size=0)
     schemanet.fit(X, y)
-    #print(torch.cuda.is_available())
 
+    print(schemanet.predict(X))
 
+    schemanet.print()
