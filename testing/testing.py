be68dd9cad5de3e16c4955f4b9b362917023291a
agorodetskiy <gorodetskiyandrew@gmail.com>
2019-10-17 14:43:23 +0300

    added tests for nop feature

--- testing/testing.py
+++ testing/testing.py
@@ -12,15 +12,15 @@ R = [R1, R2]
 
 W1 = np.reshape(np.array([0, 1, 0, 0, 1, 0, 0, 0, 0,
                           0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          1, 0]), (20, 1)).astype(bool)
+                          0, 1, 0]), (21, 1)).astype(bool)
 W = [W1]
 
 R1 = np.reshape(np.array([0, 0, 0, 0, 0, 0, 0, 0, 1,
                           0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0]), (20, 1)).astype(bool)
+                          0, 0, 0]), (21, 1)).astype(bool)
 
 R2 = np.reshape(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0,
                           0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0]), (20, 1)).astype(bool)
+                          0, 0, 0]), (21, 1)).astype(bool)
 
 R = [R1, R2]
