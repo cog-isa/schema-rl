bd1281a370f15eb641b72e1bb81b5f26e6682bf7
agorodetskiy <gorodetskiyandrew@gmail.com>
2020-05-11 17:49:42 +0300

    removed outdated code

--- play.py
+++
@@ -1,8 +0,0 @@
-from model.schemanet import SchemaNet
-
-from player import Player
-if __name__ == '__main__':
-    model = SchemaNet()
-    reward_model = SchemaNet()
-    standard_player = Player(model, reward_model)
-    standard_player.play(step_num=200, log=True, cheat=False)
